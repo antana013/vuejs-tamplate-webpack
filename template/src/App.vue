b934b7b3ffa05161005be64253d26e318082dc69
Chawinrot Niwateak <chawinrot.n@gmail.com>
2017-04-26 22:36:39 +0700

    [-] command : remove vue-router confirmation and added vuex, vue-router-sync and vue-strap.

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,24 +1,18 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    {{#router}}
     <router-view></router-view>
-    {{else}}
-    <hello></hello>
-    {{/router}}
   </div>
 </template>
 
 <script>
-{{#unless router}}
 import Hello from './components/Hello'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
-{{/unless}}
 export default {
-  name: 'app'{{#router}}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
+  name: 'app'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{else}},
   components: {
     Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}{{/router}}
+  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 </script>
 
